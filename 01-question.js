7998795ca60d7ef11fc6872e1279d232a4f5b52e
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-05-09 23:46:13 +0530

    initial commit

---
+++ 01-question.js
@@ -0,0 +1,20 @@
+// === Program to Add Two Numbers === //
+
+const num1 = 5;
+const num2 = 25;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum 
+console.log(`The sum of ${num1} and ${num2} is: ${sum}`); //The sum of 5 and 25 is: 30
+
+const prompt = require('prompt-sync')();
+const num3 = parseInt(prompt('Enter the first number '));
+const num4 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum1 = num3 + num4;
+
+// display the sum
+console.log(`The sum of ${num3} and ${num4} is ${sum1}`);
