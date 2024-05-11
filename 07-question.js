22f4ad09836dfb3722dfb59e67378b6430e7af60
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-05-11 22:31:10 +0530

    initial commit

---
+++ 07-question.js
@@ -0,0 +1,17 @@
+// === Program to Convert Kilometers to Miles === //
+
+// "Formula" ::: miles = kilometers * 0.621371
+
+const kilometers = 2.2;
+
+// conversion factor
+const factor = 0.621371
+
+// calculate miles
+const miles = kilometers * factor
+
+console.log(`${kilometers} kilometers is equal to ${miles} miles.`);
+// 2.2 kilometers is equal to 1.3670162000000001 miles.
+
+
+// To convert miles to kilometers ::: kilometers = miles / factor
