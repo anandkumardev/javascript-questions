7341d00a6258a1d21d4b11647504ffb4e09e629b
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-09-01 22:42:36 +0530

    initial commit

---
+++ 01-question.js
@@ -0,0 +1,10 @@
+// === Program to Add Two Numbers === //
+
+const num1 = 5;
+const num2 = 25;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum 
+console.log(`The sum of ${num1} and ${num2} is: ${sum}`); //The sum of 5 and 25 is: 30
