7069aa54ff30b1a78e6aceb9c1c06c1c2c831a71
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-09-02 15:44:13 +0530

    initial commit

---
+++ 03-question.js
@@ -0,0 +1,13 @@
+// === Program to Find Square Root of Different Data Types === //
+
+const number1 = 2.25;
+const number2 = -4;
+const number3 = 'Namaskaram';
+
+const result1 = Math.sqrt(number1);
+const result2 = Math.sqrt(number2);
+const result3 = Math.sqrt(number3);
+
+console.log(`The square root of ${number1} is ${result1}`);   // The square root of 2.25 is 1.5
+console.log(`The square root of ${number2} is ${result2}`);   // The square root of -4 is NaN
+console.log(`The square root of ${number3} is ${result3}`);   // The square root of Namaskaram is NaN
