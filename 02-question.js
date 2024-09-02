891e13273da4cbc24f4e4c623e2bfa451f7b90ea
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-09-02 15:39:18 +0530

    initial commit

---
+++ 02-question.js
@@ -0,0 +1,6 @@
+// === Program to Find the Square Root ==== //
+
+const number = 225;
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);  // The square root of 225 is 15
