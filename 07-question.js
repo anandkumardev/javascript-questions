9ff78ecd0bb7365b63e34ad94f0ae4630a794c46
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-09-03 11:58:39 +0530

    initial commit

---
+++ 07-question.js
@@ -0,0 +1,16 @@
+// === Program to Convert Celsius to Fahrenheit === //
+
+// "Formula" ::: fahrenheit = celsius * 1.8 + 32
+
+
+const celsius = 55;
+
+// calculate fahrenheit
+const fahrenheit = (celsius * 1.8) + 32
+
+// display the result
+console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`); // 55 degree celsius is equal to 131 degree fahrenheit.
+
+
+
+// convert fahrenheit value to celsius using the formula :::: celsius = (fahrenheit - 32) / 1.8
