8a8dd2b692db0f0d898b816aec6a6864a50839a3
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-09-04 22:28:23 +0530

    initial commit

---
+++ 13-question.js
@@ -0,0 +1,39 @@
+// === Program to Print All Prime Numbers in an Interval === //
+
+const prompt = require('prompt-sync')(); // required to use prompt
+
+// take input from the user
+const lowerNumber = parseInt(prompt('Enter lower number: '));
+const higherNumber = parseInt(prompt('Enter higher number: '));
+
+console.log(`The prime numbers between ${lowerNumber} and ${higherNumber} are:`);
+
+// looping from lowerNumber to higherNumber
+for (let i = lowerNumber; i <= higherNumber; i++) {
+    let flag = 0;
+
+    // looping through 2 to user input number
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    // if number greater than 1 and not divisible by other numbers
+    if (i > 1 && flag == 0) {
+        console.log(i);
+    }
+}
+
+
+
+// Enter lower number: 2
+// Enter higher number : 10
+
+/* The prime numbers between 2 and 10 are:
+2
+3
+5
+7
+*/
