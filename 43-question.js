a7992edbc002d2c2869ae53e423be5b9028d45ad
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-09-26 11:45:53 +0530

    initial commit

---
+++ 43-question.js
@@ -0,0 +1,28 @@
+// === Program to Remove a Property from an Object === //
+
+// creating an object
+const student = { 
+    name: 'John',
+    age: 20,
+    hobbies: ['reading', 'games', 'coding'],
+    greet: function() {
+        console.log('Hello everyone.');
+    },
+    score: {
+        maths: 90,
+        science: 80
+    }
+};
+
+// deleting a property from an object
+delete student.greet;
+delete student['score'];
+
+console.log(student);
+
+/* {
+    age: 20,
+    hobbies: ["reading", "games", "coding"],
+    name: "John"
+  }
+*/
