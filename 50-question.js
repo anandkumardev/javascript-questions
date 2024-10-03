8fd4a442d5ec0e46c67de5fe125153eba4380d6c
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-10-03 11:10:22 +0530

    initial commit

---
+++ 50-question.js
@@ -0,0 +1,23 @@
+// === Program to Generate Random String === //
+
+
+// declare all characters
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+
+console.log(generateString(5));     // nKEq3
+
+
+// :::=>  Using Built-in Methods
+
+const result = Math.random().toString(36).substring(2,7);
+console.log(result);  // gyjvo
