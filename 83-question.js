c505904dcb57ce17138648568443d40d324fc438
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-11-05 20:18:07 +0530

    initial commit

---
+++ 83-question.js
@@ -0,0 +1,12 @@
+// === Program to Generate a Random Number Between Two Numbers === //
+
+const prompt = require('prompt-sync')(); // required to use prompt
+
+const min = parseInt(prompt("Enter a min value: "));
+const max = parseInt(prompt("Enter a max value: "));
+
+// generating a random number
+const a = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// display a random number
+console.log(`Random value between ${min} and ${max} is ${a}`);
