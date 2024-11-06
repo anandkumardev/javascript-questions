a0f9ac1acbf7aa5cc71731575e61750e27139114
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-11-06 23:43:43 +0530

    initial commit

---
+++ 84-question.js
@@ -0,0 +1,12 @@
+// === Program To Get The Current URL === //
+
+// program to get the URL
+
+const url1 = window.location.href;
+const url2 = document.URL;
+console.log(url1);
+console.log(url2);
+
+/* Output : https://www.google.com/
+            https://www.google.com/
+*/
