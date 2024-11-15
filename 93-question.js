655d11f65c756ea6df5b56f3d8d8e688efbd82d5
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-11-15 23:01:41 +0530

    initial commit

---
+++ 93-question.js
@@ -0,0 +1,26 @@
+// === rogram to Pass a Function as Parameter === //
+
+// program to pass a function as a parameter
+
+function greet() {
+    return 'Hello';
+}
+
+// passing function greet() as a parameter
+function name(user, func)
+{
+
+    // accessing passed function
+    const message = func();
+
+    console.log(`${message} ${user}`);
+}
+
+name('John', greet);
+name('Jack', greet);
+name('Sara', greet);
+
+/*  Hello John
+    Hello Jack
+    Hello Sara
+*/
