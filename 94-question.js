627a90e4d0c1792a312f08fa9b08cfd09161db43
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-11-16 23:45:18 +0530

    initial commit

---
+++ 94-question.js
@@ -0,0 +1,16 @@
+// === Program to Get the Dimensions of an Image === //
+
+const img = new Image();
+
+// get the image
+img.src = '//cdn.programiz.com/sites/tutorial2program/files/cover-artwork.png';
+
+// get height and width
+img.onload = function() {
+    console.log('width ' + this.width)
+    console.log('height '+ this.height);
+}
+
+
+/* width 1040
+   height 922 */
