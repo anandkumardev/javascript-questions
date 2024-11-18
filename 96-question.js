9bea89b594aa2a351d9c965ac7dc068070b0363e
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-11-18 19:13:14 +0530

    initial commit

---
+++ 96-question.js
@@ -0,0 +1,39 @@
+// === Program to Write to Console === //
+
+// passing number 
+console.log(8);
+
+// passing string
+console.log('hello');
+
+// passing variable
+const x = 'hello';
+console.log(x);
+
+// passing function
+function sayName() {
+    return 'Hello John';
+}
+console.log(sayName());
+
+// passing string and a variable
+const name = 'John';
+console.log('Hello ' + name);
+
+// passing object
+let obj = {
+    name: 'John',
+    age: 28
+}
+console.log(obj);
+
+/* Output: 8
+            hello
+            hello
+            Hello John
+            {
+                age: 28,
+                Hello John
+                name: "John"
+            }
+*/
