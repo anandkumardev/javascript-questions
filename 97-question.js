11c704f7a94dcbe770833900fc1ae2b06c9f7256
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-11-19 19:26:12 +0530

    initial commit

---
+++ 97-question.js
@@ -0,0 +1,14 @@
+// === Program to Convert Date to Number === //
+
+// create date
+const d1 = new Date();
+console.log(d1);
+
+// converting to number
+const result = d1.getTime();
+console.log(result);
+
+/*
+    output: 2024-11-19T13:54:59.382Z
+            1732024499382
+*/
